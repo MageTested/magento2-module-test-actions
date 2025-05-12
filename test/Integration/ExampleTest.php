771c4f795165f145e45964e3ef0af3ebf11cddda
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2025-05-12 14:32:33 -0600

    Create example test

---
+++ test/Integration/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JosephLeedy\TestActions\Test\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
+{
+    public function testItWorks(): void
+    {
+        self::assertTrue(true);
+    }
+}
