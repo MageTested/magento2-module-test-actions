74103721929fdf0f84be7e6f410961856a8ee807
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2025-05-13 10:14:09 -0600

    Create test for Example View Model

---
+++ test/Integration/ViewModel/ExampleViewModelTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JosephLeedy\TestActions\Test\Integration\ViewModel;
+
+use JosephLeedy\TestActions\ViewModel\ExampleViewModel;
+use Magento\Framework\App\Area;
+use Magento\Store\Model\ScopeInterface;
+use Magento\TestFramework\Fixture\AppArea;
+use Magento\TestFramework\Fixture\Config;
+use Magento\TestFramework\Helper\Bootstrap;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+#[AppArea(Area::AREA_FRONTEND)]
+final class ExampleViewModelTest extends TestCase
+{
+    #[Test]
+    #[Config('general/store_information/name', 'Test Store', ScopeInterface::SCOPE_STORE, 'default')]
+    public function it_gets_the_configured_store_name(): void
+    {
+        $objectManager = Bootstrap::getObjectManager();
+        /** @var ExampleViewModel $exampleViewModel */
+        $exampleViewModel = $objectManager->create(ExampleViewModel::class);
+
+        $expectedStoreName = 'Test Store';
+        $actualStoreName = $exampleViewModel->getStoreName();
+
+        self::assertSame($expectedStoreName, $actualStoreName);
+    }
+}
